5bb8ea1fd2584e48b1cdb46683544a161a2bd12f
gaoyx9 <gaoyx9@motorola.com>
2020-11-05 14:21:48 +0800

    导出两个版本之间的CR列表

--- get_change_list.py
+++ get_change_list.py
@@ -21,13 +21,14 @@ gerrit_server="gerrit.mot.com"
 username="gaoyx9"
 password="gyx050400??"
 root=os.getcwd()
+commit_list=[]
 
-def set_style(name,height,back_color,bold=False):
+def set_style(name,height,bold=False):
     style=xlwt.XFStyle()
     pattern=xlwt.Pattern()
     pattern.pattern=xlwt.Pattern.SOLID_PATTERN
     #设置背景颜色
-    pattern.pattern_fore_colour=17
+    pattern.pattern_fore_colour=3
     style.pattern=pattern
     #设置边框(为实线)
     borders=xlwt.Borders()
@@ -42,18 +43,27 @@ def set_style(name,height,back_color,bold=False):
     font.size=500
     return style
 
-def in_process():
-	global commit_list
-	global project_path
-	commit_list=[]
+def write_excel_head():
+	global f
+	global sheet1
+	f=xlwt.Workbook(encoding="utf-8")
+	sheet1=f.add_sheet("change_list",cell_overwrite_ok=True)
+	row0=[u'CR',u'DESCRIPTION',u'COMPONENTS',u'ASSIGN',u"REPO PATH",u"COMMIT"]
+
+	for row in range(0,len(row0)):
+		sheet1.write(0,row,row0[row],set_style('Times New Roman',400,True))
+
     #将changed_from的行的下标放入数组中
 	for index,line in enumerate(flist):
 		if line!="":
 			if "changed from" in line:
 				change_from_number_array.append(index)
 
+def get_commit():
     #change_from_number_array 中取出 change_from_number_array自己的下标, 和存放的 flist中 changed from 的下标
+	commit_index=0
 	for change_index,change_number_index in enumerate(change_from_number_array):
+		commit_list=[]
 		project_path=flist[change_number_index].split()[0]
 		ssh1="repo list -n %s" % (project_path)
 		process=subprocess.Popen(ssh1,shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -75,38 +85,56 @@ def in_process():
 				for commit_line in commit_message:
 					commit_list.append(commit_line.split()[1])
 
-def write_excel():
-	f=xlwt.Workbook(encoding="utf-8")
-	sheet1=f.add_sheet("change_list",cell_overwrite_ok=True)
-	row0=[u'CR',u'DESCRIPTION',u'COMPONENTS',u'ASSIGN',u"REPO PATH",u"COMMIT"]
+		for commit in commit_list:
+			cmd1="ssh -p 29418 %s gerrit query commit:%s --format JSON | egrep 'project|branch|subject' | awk 'NR==1'" % (gerrit_server,commit)
+			process=subprocess.Popen(cmd1,shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			process.wait()
+			content=process.stdout.read()
+			if content=="":
+				if os.path.isdir(project_path):
+					os.chdir(project_path)
+					a1="git log %s -n 1 --format='%s'" % (commit,"%s")
+					process=subprocess.Popen(a1,shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+					process.wait()
+					subject=process.stdout.read()
+					os.chdir(root)
+					cr=re.findall(r'IK[A-Z]*-[0-9]*',subject)
+					if len(cr)==0:
+						print("no cr")
+						sheet1.write(commit_index+1,4,project_path)
+						sheet1.write(commit_index+1,4,project_path)
+						commit_index+=1
+					else:
+						cr_component=response["fields"]["components"][0]["name"]
+						cr_url="https://idart.mot.com/browse/%s" % (cr)
+						url="http://idart.mot.com/rest/api/2/issue/%s" % (cr)
+						response=requests.get(url,auth=(username,password)).json()
+						cr_description=response["fields"]["summary"]
+						cr_component=response["fields"]["components"]
+						if len(cr_component)==0:
+							print("no component")
+						else:
+							cr_component=response["fields"]["components"][0]["name"]
+						cr_assign=response["fields"]["assignee"]["displayName"]
+						sheet1.write(commit_index+1,0,cr)
+						sheet1.write(commit_index+1,1,cr_description)
+						sheet1.write(commit_index+1,2,cr_component)
+						sheet1.write(commit_index+1,3,cr_assign)
+						sheet1.write(commit_index+1,5,commit)
+						commit_index+=1
+			else:
+				message=json.loads(content)
+				repo_path=message["project"]
+				subject=message["subject"]
 
-	for row in range(0,len(row0)):
-		sheet1.write(0,row,row0[row],set_style('Times New Roman',400,17,True))
-
-	for commit_index,commit in enumerate(commit_list):
-		cmd1="ssh -p 29418 %s gerrit query commit:%s --format JSON | egrep 'project|branch|subject' | awk 'NR==1'" % (gerrit_server,commit)
-		process=subprocess.Popen(cmd1,shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		process.wait()
-		content=process.stdout.read()
-		if content=="":
-			if os.path.isdir(project_path):
-				os.chdir(project_path)
-				a1="git log %s -n 1 --format='%s'" % (commit,"%s")
-				process=subprocess.Popen(a1,shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-				process.wait()
-				subject=process.stdout.read()
-				os.chdir(root)
 				cr=re.findall(r'IK[A-Z]*-[0-9]*',subject)
 				if len(cr)==0:
-					print("no content:no cr")
-					sheet1.write(commit_index+1,0)
-					sheet1.write(commit_index+1,1)
-					sheet1.write(commit_index+1,2)
-					sheet1.write(commit_index+1,3)
+					print("no cr")
 					sheet1.write(commit_index+1,4,project_path)
 					sheet1.write(commit_index+1,5,commit)
+					commit_index+=1
 				else:
-					cr_component=response["fields"]["components"][0]["name"]
+					cr=cr=re.findall(r'IK[A-Z]*-[0-9]*',subject)[0]
 					cr_url="https://idart.mot.com/browse/%s" % (cr)
 					url="http://idart.mot.com/rest/api/2/issue/%s" % (cr)
 					response=requests.get(url,auth=(username,password)).json()
@@ -117,48 +145,17 @@ def write_excel():
 					else:
 						cr_component=response["fields"]["components"][0]["name"]
 					cr_assign=response["fields"]["assignee"]["displayName"]
+					#print(line_index+1,cr,cr_description,cr_component,cr_assign)
 					sheet1.write(commit_index+1,0,cr)
 					sheet1.write(commit_index+1,1,cr_description)
 					sheet1.write(commit_index+1,2,cr_component)
 					sheet1.write(commit_index+1,3,cr_assign)
-					sheet1.write(commit_index+1,4,project_path)
+					sheet1.write(commit_index+1,4,repo_path)
 					sheet1.write(commit_index+1,5,commit)
-		else:
-			message=json.loads(content)
-			repo_path=message["project"]
-			subject=message["subject"]
-			cr=re.findall(r'IK[A-Z]*-[0-9]*',subject)
-			if len(cr)==0:
-				print("no cr")
-				sheet1.write(commit_index+1,0)
-				sheet1.write(commit_index+1,1)
-				sheet1.write(commit_index+1,2)
-				sheet1.write(commit_index+1,3)
-				sheet1.write(commit_index+1,4,repo_path)
-				sheet1.write(commit_index+1,5,commit)
-			else:
-				print(commit_index)
-				cr=cr=re.findall(r'IK[A-Z]*-[0-9]*',subject)[0]
-				cr_url="https://idart.mot.com/browse/%s" % (cr)
-				url="http://idart.mot.com/rest/api/2/issue/%s" % (cr)
-				response=requests.get(url,auth=(username,password)).json()
-				cr_description=response["fields"]["summary"]
-				cr_component=response["fields"]["components"]
-				if len(cr_component)==0:
-					print("no component")
-				else:
-					cr_component=response["fields"]["components"][0]["name"]
-				cr_assign=response["fields"]["assignee"]["displayName"]
-				sheet1.write(commit_index+1,0,cr)
-				sheet1.write(commit_index+1,1,cr_description)
-				sheet1.write(commit_index+1,2,cr_component)
-				sheet1.write(commit_index+1,3,cr_assign)
-				sheet1.write(commit_index+1,4,repo_path)
-				sheet1.write(commit_index+1,5,commit)
+					commit_index+=1
 		f.save("change_list.xls")
 
-	
 #==============================================
 if __name__=="__main__":
-	in_process()
-	write_excel()
+	write_excel_head()
+	get_commit()
