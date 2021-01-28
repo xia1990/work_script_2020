347239bbf7b7a19b7cb3e2264228e57c000f536b
xia1990 <18016074089@163.com>
2021-01-28 17:42:40 +0800

    监测continuous_job如果构建成功则触发daily_job

--- dg_son.py
+++ dg_son.py
@@ -3,13 +3,15 @@
 import jenkins
 import sys
 import time
+import os
 
-server = jenkins.Jenkins('http://192.168.137.19:8080',username="jenkins",password="jenkins",timeout=10)
-JOB_BUILD_ID_STR=sys.argv[2]
-JOB_BUILD_ID=int(JOB_BUILD_ID_STR)
-JOB_NAME=sys.argv[1]
-SON_JOB=sys.argv[3]
+server = jenkins.Jenkins('http://192.168.56.101:8088',username="jenkins",password="jenkins",timeout=10)
+JOB_BUILD_ID=os.environ["JOB_BUILD_ID"]
+JOB_BUILD_ID=int(JOB_BUILD_ID)
+JOB_NAME=os.environ["JOB_NAME"]
+SON_JOB=os.environ["SON_JOB"]
 level=0 #这个就是当前build失败后,用来定位下一个build的
+
 
 def getBuildQueueStatus(job_name):  #看看是否这个JOB在排队
 	queue_list=server.get_queue_info()
@@ -21,6 +23,7 @@ def getBuildQueueStatus(job_name):  #看看是否这个JOB在排队
 				return true #排队JOB里有一个我们的JOB (有一个就可以了)
 		return false   #排队的JOB里没有我们这个JOB
 
+#getBuildResult("continuous",32)
 def getBuildResult(job_name,job_build_id):
 	try:
 		result=server.get_build_info(job_name,int(job_build_id))
@@ -32,15 +35,21 @@ def getBuildResult(job_name,job_build_id):
 		else:
 			print("can not file %s %s" % (job_name,job_build_id)) #没有找到这个 BUILD
 			sys.exit(0)
-			
+		print("*********************")	
 
 def build_son(result,level):
 	print(JOB_NAME,JOB_BUILD_ID+level,result["result"]) #打印当前build是否成功执行
+	Flag="false"
+	param_dict={'BUILD_NUM_PROMOTION':JOB_BUILD_ID+level}
 	time.sleep(1)
 	level=level+1
 	if result["result"] == "SUCCESS":
 		print("start son")
-		#server.build_job(SON_JOB) #可以启动你需要启动的JOB
+		server.build_job(SON_JOB,parameters=param_dict)
+		Flag="true"
+		if Flag==true:
+			sys.exit(0)
+        #可以启动你需要启动的JOB
 	elif result["result"] == "FAILURE": 
 		subresult=getBuildResult(JOB_NAME,JOB_BUILD_ID+level)
 		build_son(subresult,level)
