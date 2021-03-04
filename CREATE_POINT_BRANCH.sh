e65e2ca22a07c13dc0e267aa5484c8e8ee4d0cff
xia1990 <18016074089@163.com>
2021-03-04 15:05:08 +0800

    添加拉分支脚本
    当输入UPDATE_ID参数时才会拉BUILD_ID的分支，才会修改BUILD_ID的XML

--- CREATE_POINT_BRANCH.sh
+++ CREATE_POINT_BRANCH.sh
@@ -2,6 +2,8 @@
 
 PROJECT=$1
 BRANCH=$2
+TAG=$3
+UPDATE_ID=$4
 MANIFEST="R"
 POINT="False"
 PRODUCT="IBIZA"
@@ -16,64 +18,113 @@ fi
 case $MANIFEST in
 Q)
     MANIFEST="manifest/q"
-    NEW_MANIFEST="r-6125.xml"
+    XML="r-6125.xml"
     ;;  
 R)
     MANIFEST="manifest/r"
     if [ $PRODUCT=="IBIZA" ];then
-    	NEW_MANIFEST="r-qsm2021.xml"
+    	XML="r-qsm2021.xml"
     elif [ $PRODUCT=="CAPRI" ];then
-    	NEW_MANIFEST="r-qsm2020.xml"
+    	XML="r-qsm2020.xml"
     elif [ $PRODUCT=="CAPRIP" ];then
-    	NEW_MANIFEST="r-qsm2020.xml"
+    	XML="r-qsm2020.xml"
     else
-    	echo "$NEW_MANIFEST"
+    	echo "$XML"
     fi
     ;;  
 esac
 
-if [ -d manifest ];then
-	rm -rf manifest
-	git clone ssh://gerrit.mot.com/home/repo/dev/platform/android/platform/${MANIFEST} -b $BRANCH manifest
-else 
-	git clone ssh://gerrit.mot.com/home/repo/dev/platform/android/platform/${MANIFEST} -b $BRANCH manifest
-fi
+function Create_ManifestBranch(){
+	cd $ROOTPATH
+		if [ -d manifest ];then
+			rm -rf manifest
+			git clone ssh://gerrit.mot.com/home/repo/dev/platform/android/platform/${MANIFEST} manifest
+		else 
+			git clone ssh://gerrit.mot.com/home/repo/dev/platform/android/platform/${MANIFEST} manifest
+		fi
 
-function main(){
+
+		cd ./manifest
+			git checkout $TAG
+			git checkout -b $BRANCH
+			git push origin $BRANCH:$BRANCH
+
+			cp sha1_embedded_manifest.xml  $XML
+			git add $XML
+			git commit -m "pull point branch on $BRANCH"
+			git push origin HEAD:refs/heads/$BRANCH
+		cd -
+	cd -
+}
+
+function Pull_Branch(){
 	cd $ROOTPATH/manifest
-		REVISION=`cat $NEW_MANIFEST | grep -w $PROJECT\" | grep -aoe "revision=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
-		UPSTREAM=`cat $NEW_MANIFEST | grep -w $PROJECT\" | grep -aoe "upstream=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
+		if [ $UPDATE_ID ];then
+			REVISION1=`cat $XML | grep "motorola/build_ids" | grep -aoe "revision=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
+			UPSTREAM1=`cat $XML | grep "motorola/build_ids" | grep -aoe "upstream=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
+			#删掉最后一个/  及其左边的字符串
+			UP=${UPSTREAM1##*/}
+			P="home\/repo\/dev\/platform\/android\/motorola\/build_ids"
+			if [ "$POINT" == "False" ];then
+				ssh -p 29418 gerrit.mot.com gerrit create-branch home/repo/dev/platform/android/motorola/build_ids $BRANCH $REVISION1
+				if [ $?==0 ];then
+					echo "CREATE BUILD ID BRANCH SUCESSFULLY"
+				fi
+				echo "begin update xml"
+				echo $REVISION1
+				echo $UPSTREAM2
+				sed -i "s#revision=\"$REVISION1\"#revision=\"$BRANCH\"#" $XML
+				sed -i "/$P\"/s#upstream=\"$UPSTREAM1\"##"  $XML
+				git diff $XML
+			else
+				ssh -p 29418 gerrit.mot.com gerrit create-branch home/repo/dev/platform/android/motorola/build_ids $BRANCH/$UP $REVISION1
+				if [ $? == 0 ];then
+					echo "CREATE BUILD ID BRANCH SUCESSFULLY"
+				fi
+				sed -i "s#revision=\"$REVISION1\"#revision=\"$BRANCH\/$UP\"#" $XML
+				sed -i "/$P\"/s#upstream=\"$UPSTREAM1\"##" $XML
+				git diff $XML
+			fi
+		else 
+			continue
+		fi
+	cd -
+}
+
+function Pull_Branch_Main(){
+	cd $ROOTPATH/manifest
+		REVISION2=`cat $XML | grep -w $PROJECT\" | grep -aoe "revision=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
+		UPSTREAM2=`cat $XML | grep -w $PROJECT\" | grep -aoe "upstream=[a-zA-Z0-9\.\"\"\/\-]*" | awk -F'"' '{print $2}'`
 		#删掉最后一个/  及其左边的字符串
-		UP=${UPSTREAM##*/}
-		#把PROJECT中的/全部替换成\/
-		P=${PROJECT//\//\\\/}
+		UP=${UPSTREAM2##*/}
+		P=${PROJECT//\//\\/}
 
 		if [ "$POINT" == "False" ];then
-			ssh -p 29418 gerrit.mot.com gerrit create-branch $PROJECT $BRANCH $REVISION
+			ssh -p 29418 gerrit.mot.com gerrit create-branch $PROJECT $BRANCH $REVISION2
 			if [ $?==0 ];then
 				echo "分支创建成功！"
 			fi
 			echo "begin update xml"
-			echo $REVISION
-			echo $UPSTREAM
-			sed -i "s#revision=\"$REVISION\"#revision=\"$BRANCH\"#" $NEW_MANIFEST
-			P=${PROJECT//\//\\\/}
-			#sed -i "/$P\"/s#upstream=\"$UPSTREAM\"#upstream=\"$BRANCH\"#"  $NEW_MANIFEST
-			sed -i "/$P\"/s#upstream=\"$UPSTREAM\"##"  $NEW_MANIFEST
-			git diff $NEW_MANIFEST
+			echo $REVISION2
+			echo $UPSTREAM2
+			sed -i "s#revision=\"$REVISION2\"#revision=\"$BRANCH\"#" $XML
+			#sed -i "/$P\"/s#upstream=\"$UPSTREAM\"#upstream=\"$BRANCH\"#"  $XML
+			sed -i "/$P\"/s#upstream=\"$UPSTREAM2\"##"  $XML
+			git diff $XML
 		else
-			ssh -p 29418 gerrit.mot.com gerrit create-branch $PROJECT $BRANCH/$UP $REVISION
-			if [ $?==0 ];then
+			ssh -p 29418 gerrit.mot.com gerrit create-branch $PROJECT $BRANCH/$UP $REVISION2
+			if [ $? == 0 ];then
 				echo "分支创建成功！"
 			fi
-			sed -i "s#revision=\"$REVISION\"#revision=\"$BRANCH\/$UP\"#" $NEW_MANIFEST
-			sed -i "/$P\"/s#upstream=\"$UPSTREAM\"##" $NEW_MANIFEST
-			git diff $NEW_MANIFEST
+			sed -i "s#revision=\"$REVISION2\"#revision=\"$BRANCH\/$UP\"#" $XML
+			sed -i "/$P\"/s#upstream=\"$UPSTREAM2\"##" $XML
+			git diff $XML
 		fi
-
 	cd -
 }
 
 
 ################################################
-main
+Create_ManifestBranch
+Pull_Branch
+Pull_Branch_Main
